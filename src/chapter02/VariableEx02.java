4960511cde4b50b0075f57fc6ccbccbb9ce5e6d2
서두식 <@DESKTOP-UBQIAPB>
2022-11-11 16:52:19 +0900

    사칙연산 출력해보기

---
+++ src/chapter02/VariableEx02.java
@@ -0,0 +1,12 @@
+package chapter02;
+
+public class VariableEx02 {
+	public static void main(String[] args) {
+		
+		// 사칙연산 출력해보기
+		System.out.println(5+3); // 결과로 8이 출력된다.
+		System.out.println(5-3); // 결과로 2가 출력된다.
+		System.out.println(5*3); // 결과로 15가 출력된다.
+		System.out.println(5/3); // 결과로 1이 출력된다.
+	}
+}
